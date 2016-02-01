4b0ca2550b9f57f15ea5101fbf9784cd9a91b168
hxc05 <hxc05@yahoo.com>
2016-01-31 20:48:36 -0500

    Create AccountType.cs

---
+++ abc-bank/AccountType.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abc_bank
+{
+	public enum AccountType
+	{
+		CHECKING;
+		SAVINGS;
+		MAXI_SAVINGS;
+	}
+}
